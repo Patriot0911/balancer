eb62572749e9fd870143c3b792175bfe6c3e7270
aim4ik <rusak.yaroslav@lll.kpi.ua>
2024-05-30 09:07:04 +0300

    Added error notifications when team balance fails

--- src/components/BalanceActions/index.tsx
+++ src/components/BalanceActions/index.tsx
@@ -3,31 +3,24 @@
 import { IBalanceActionsProps, IPlayer, ITeamInfo } from '@/types';
 import { addTeam, clearAllTeams } from '@/redux/features/teams-slice';
 import ClassicButton from '../ui/ClassicButton';
+import ErrorAlert from "../ui/ErrorInfo";
 import { balanceByPair } from '@/scripts/balanceByPair';
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 const BalanceActions = ({ players }: IBalanceActionsProps) => {
+    const [error, setError] = useState<string | null>();
     const dispatch = useDispatch();
 
-    const isPlayerInTeam = (player: IPlayer, teams: ITeamInfo[]): boolean => {
-        for (const team of teams) {
-            if (
-                !team.tank.includes(player)
-                && !team.damage.includes(player)
-                && !team.support.includes(player)
-            ) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
     const balanceHandle = () => {
+        const teamsNumber = Math.floor(players.length / 5);
+        if (teamsNumber < 2) {
+            return setError("Not enough players to form teams");
+        }
         const teams = balanceByPair(players, 2/*Math.floor(players.length / 5)*/);
-        console.log(teams);
-        if (!teams)
-            return console.log('Something went wrong with team generations');
+        if (!teams) {
+            return setError("Something went wrong with team formation");
+        }
         dispatch(clearAllTeams());
         for (const team of teams) {
             dispatch(addTeam(team));
@@ -35,12 +28,21 @@ const BalanceActions = ({ players }: IBalanceActionsProps) => {
     };
 
     return (
-        <div>
-            <ClassicButton
-                onClick={balanceHandle}
-                text={'Balance by pair'}
-            />
-        </div>
+        <>
+            {
+                error ?
+                    <ErrorAlert
+                        text={error}
+                    /> :
+                    <br />
+            }
+            <div>
+                <ClassicButton
+                    onClick={balanceHandle}
+                    text={'Balance by pair'}
+                />
+            </div>
+        </>
     );
 };
 
--- src/components/TeamsBlock/index.tsx
+++ src/components/TeamsBlock/index.tsx
@@ -5,40 +5,40 @@ import { useAppSelector } from '@/redux/store';
 import { ITeamPairInfo } from '@/types';
 
 const TeamsBlock = () => {
-    const teams = useAppSelector(item => item.teamsReducer.value);
-    const teamPairs: ITeamPairInfo[] = [];
+  const teams = useAppSelector(item => item.teamsReducer.value);
+  const teamPairs: ITeamPairInfo[] = [];
 
-    for (let i = 0; i < teams.length; i += 2) {
-        if (i + 1 < teams.length) {
-            teamPairs.push({
-                teamA: teams[i],
-                teamB: teams[i + 1],
-            });
-        }
+  for (let i = 0; i < teams.length; i += 2) {
+    if (i + 1 < teams.length) {
+      teamPairs.push({
+        teamA: teams[i],
+        teamB: teams[i + 1],
+      });
     }
-    return (
-        <div
-            className={'teams-block'}
-        >
-            {
-                teamPairs.map((team) => (
-                    <>
-                        <Team
-                            name={`Team ${team.teamA.tank[0].name}`}
-                            teamInfo={team.teamA}
-                            key={`Team ${team.teamB.tank[0].name}`}
-                        />
-                        <h3>VS</h3>
-                        <Team
-                            name={`Team ${team.teamB.tank[0].name}`}
-                            teamInfo={team.teamB}
-                            key={`Team ${team.teamB.tank[0].name}`}
-                        />
-                    </>
-                ))
-            }
-        </div>
-    );
+  }
+  return (
+    <div
+      className={'teams-block'}
+    >
+      {
+        teamPairs.map((team) => (
+          <>
+            <Team
+              name={`Team ${team.teamA.tank[0].name}`}
+              teamInfo={team.teamA}
+              key={`Team ${team.teamA.tank[0].name}`}
+            />
+            <h3>VS</h3>
+            <Team
+              name={`Team ${team.teamB.tank[0].name}`}
+              teamInfo={team.teamB}
+              key={`Team ${team.teamB.tank[0].name}`}
+            />
+          </>
+        ))
+      }
+    </div>
+  );
 };
 
 export default TeamsBlock;
